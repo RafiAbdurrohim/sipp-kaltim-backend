d33484d7fc76979921c638b2b2b3fd028de8f34f
RafiAbdurrohim <abdurrohimrafi@gmail.com>
2026-03-04 10:26:12 +0800

    fix port

--- src/app.js
+++ src/app.js
@@ -12,5 +12,5 @@ app.use("/api/perkara", require("./routes/perkara"));
 
 app.get("/api/health", (req, res) => res.json({ status: "ok", message: "SIPP Backend berjalan!" }));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`🚀 Server berjalan di http://localhost:${PORT}`));
